b3f097a4eac84da6e48c679fb24ffef1e7d1c018
Patrick Barrett <patrick@yeet.cx>
2025-03-12 12:30:00 -0500

    Add existing dnssnoop code

---
+++ include/dnssnoop.h
@@ -0,0 +1,108 @@
+#ifndef __DNSSNOOP_H__
+#define __DNSSNOOP_H__
+
+#include <vmlinux.h>
+
+#define ETH_P_IP 0x0800
+
+#define COMM_BUF_SIZE 16
+#define NAME_BUF_SIZE 256
+#define CGROUP_BUF_SIZE 512
+
+#define RINGBUF_SIZE 1024
+#define LRU_HASH_SIZE 1024
+
+#define DNS_QUERY_CODE 0
+
+#define DNS_FLAG_QR(flags) (((u16) (flags) & 0x8000) >> 15)
+#define DNS_FLAG_OPCODE(flags) (((u16) (flags) & 0x7800) >> 11)
+#define DNS_FLAG_AA(flags) (((u16) (flags) & 0x0400) >> 10)
+#define DNS_FLAG_TC(flags) (((u16) (flags) & 0x0200) >> 9)
+#define DNS_FLAG_RD(flags) (((u16) (flags) & 0x0100) >> 8)
+#define DNS_FLAG_RA(flags) (((u16) (flags) & 0x0080) >> 7)
+#define DNS_FLAG_Z(flags) (((u16) (flags) & 0x0040) >> 6)
+#define DNS_FLAG_AD(flags) (((u16) (flags) & 0x0020) >> 5)
+#define DNS_FLAG_CD(flags) (((u16) (flags) & 0x0010) >> 4)
+#define DNS_FLAG_RCODE(flags) ((u16) (flags) & 0x000F)
+
+struct conn_key {
+  u32 saddr;
+  u32 daddr;
+  u16 sport;
+  u16 dport;
+};
+
+struct dnshdr {
+  u16 id;
+  u16 flags;
+  u16 q_count;
+  u16 ans_count;
+  u16 auth_count;
+  u16 add_count;
+};
+
+struct dns_query_internal {
+  pid_t pid;
+  pid_t tgid;
+  u64 cgroup_id;
+  u64 start_time;
+  u16 id;
+  char comm[COMM_BUF_SIZE];
+  char name[NAME_BUF_SIZE];
+  char cgroup[CGROUP_BUF_SIZE];
+} __attribute__((packed));
+
+struct dns_query {
+  pid_t pid;
+  pid_t tgid;
+  u64 cgroup_id;
+  u64 latency_ns;
+  u16 id;
+  char comm[COMM_BUF_SIZE];
+  char name[NAME_BUF_SIZE];
+  char cgroup[CGROUP_BUF_SIZE];
+} __attribute__((packed));
+
+#define READ_FROM_PACKET(type, var, data, data_len)  \
+  do {                                               \
+    if (data_len < sizeof(type)) {                   \
+      return 0;                                      \
+    }                                                \
+    bpf_probe_read_kernel(&var, sizeof(type), data); \
+    data_len -= sizeof(type);                        \
+    data += sizeof(type);                            \
+  } while (0)
+
+#define DECODE_PACKETS_UDP(sk_buff_addr, ip, udp, data, data_len, use_eth) \
+  do {                                                                     \
+    struct sk_buff skb = {};                                               \
+    bpf_probe_read_kernel(&skb, sizeof(struct sk_buff), sk_buff_addr);     \
+    data_len = skb.len;                                                    \
+    data = (void*) (long) skb.data;                                        \
+    if (use_eth) {                                                         \
+      struct ethhdr eth = {};                                              \
+      READ_FROM_PACKET(struct ethhdr, eth, data, data_len);                \
+      if (eth.h_proto != bpf_htons(ETH_P_IP)) {                            \
+        return EXIT_FAILURE;                                               \
+      }                                                                    \
+    }                                                                      \
+    if (data_len < sizeof(struct iphdr)) {                                 \
+      return EXIT_FAILURE;                                                 \
+    }                                                                      \
+    data_len -= sizeof(struct iphdr);                                      \
+    bpf_probe_read_kernel(&ip, sizeof(struct iphdr), data);                \
+    u8 ip_ihl = ip.ihl;                                                    \
+    data_len += sizeof(struct iphdr);                                      \
+    if (data_len < ip_ihl << 2) {                                          \
+      return EXIT_FAILURE;                                                 \
+    }                                                                      \
+    data += ip_ihl << 2;                                                   \
+    data_len -= ip_ihl << 2;                                               \
+    if (ip.protocol != IPPROTO_UDP) {                                      \
+      return EXIT_FAILURE;                                                 \
+    }                                                                      \
+    data_len -= sizeof(struct udphdr);                                     \
+    bpf_probe_read_kernel(&udp, sizeof(struct udphdr), data);              \
+    data += sizeof(struct udphdr);                                         \
+  } while (0)
+#endif
--- src/probe.bpf.c
+++ src/probe.bpf.c
@@ -1,28 +1,128 @@
 #include <vmlinux.h>
+
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
+
 #include <yeet/yeet.h>
 
-#define EXIT_SUCCESS 0
+#include "dnssnoop.h"
 
-struct output {
-  int pid;
-} __attribute__((packed));
+RINGBUF_CHANNEL(dns_queries_rb, RINGBUF_SIZE * sizeof(struct dns_query), dns_query);
 
-RINGBUF_CHANNEL(output_rb, 1024 * sizeof(struct output), output);
+struct {
+  __uint(type, BPF_MAP_TYPE_LRU_HASH);
+  __type(key, struct conn_key);
+  __type(value, struct dns_query_internal);
+  __uint(max_entries, LRU_HASH_SIZE);
+} conns_map SEC(".maps");
 
-SEC("tracepoint/syscalls/sys_enter_execve")
-int trace_sys_execve(struct trace_event_raw_sys_enter *ctx)
+struct dns_query_internal empty = {};
+
+SEC("tracepoint/net/net_dev_xmit")
+int trace_egress(struct trace_event_raw_net_dev_xmit* ctx)
 {
-  struct output *cmd = bpf_ringbuf_reserve(&output_rb, sizeof(struct output), 0);
-  int pid = bpf_get_current_pid_tgid() >> 32;
+  void* buff_addr = BPF_CORE_READ(ctx, skbaddr);
+  struct iphdr ip = {};
+  struct udphdr udp = {};
+  void* data = NULL;
+  u32 data_len = 0;
 
-  if (!cmd) {
-    return EXIT_SUCCESS;
+  DECODE_PACKETS_UDP(buff_addr, ip, udp, data, data_len, true);
+
+  struct dnshdr dns = {};
+  READ_FROM_PACKET(struct dnshdr, dns, data, data_len);
+
+  dns.id = bpf_ntohs(dns.id);
+  dns.flags = bpf_ntohs(dns.flags);
+  dns.q_count = bpf_ntohs(dns.q_count);
+  dns.ans_count = bpf_ntohs(dns.ans_count);
+  dns.auth_count = bpf_ntohs(dns.auth_count);
+  dns.add_count = bpf_ntohs(dns.add_count);
+
+  if (DNS_FLAG_Z(dns.flags) != 0 || DNS_FLAG_QR(dns.flags) != DNS_QUERY_CODE) {
+    return EXIT_FAILURE;
   }
 
-  cmd->pid = pid;
-  bpf_ringbuf_submit(cmd, 0);
+  struct conn_key key = {};
+  key.saddr = ip.saddr;
+  key.daddr = ip.daddr;
+  key.sport = udp.source;
+  key.dport = udp.dest;
 
+  bpf_map_update_elem(&conns_map, &key, &empty, BPF_ANY);
+
+  struct task_struct* cur_tsk = (struct task_struct*) bpf_get_current_task();
+  if (!cur_tsk) {
+    return EXIT_FAILURE;
+  }
+
+  struct dns_query_internal* cached = bpf_map_lookup_elem(&conns_map, &key);
+  if (!cached) {
+    return EXIT_FAILURE;
+  }
+  bpf_probe_read_kernel_str(&cached->name, NAME_BUF_SIZE, data);
+
+  pid_t pid = bpf_get_current_pid_tgid() >> 32;
+  cached->pid = pid;
+
+  pid_t tgid = bpf_get_current_pid_tgid();
+  cached->tgid = tgid;
+
+  cached->id = dns.id;
+  cached->start_time = bpf_ktime_get_ns();
+
+  cached->cgroup_id = bpf_get_current_cgroup_id();
+  const char* name = BPF_CORE_READ(cur_tsk, cgroups, subsys[memory_cgrp_id], cgroup, kn, name);
+  if (bpf_probe_read_kernel_str(&cached->cgroup, CGROUP_BUF_SIZE, name) < 0) {
+    bpf_map_delete_elem(&conns_map, &key);
+    return EXIT_FAILURE;
+  }
+
+  bpf_get_current_comm(cached->comm, COMM_BUF_SIZE);
+  bpf_map_update_elem(&conns_map, &key, cached, BPF_ANY);
+  return EXIT_SUCCESS;
+}
+
+SEC("tracepoint/net/netif_receive_skb")
+int trace_ingress(struct trace_event_raw_net_dev_template* ctx)
+{
+  void* buff_addr = BPF_CORE_READ(ctx, skbaddr);
+  struct iphdr ip = {};
+  struct udphdr udp = {};
+  void* data = NULL;
+  u32 data_len = 0;
+  int err;
+
+  DECODE_PACKETS_UDP(buff_addr, ip, udp, data, data_len, false);
+
+  struct conn_key key = {};
+  key.saddr = ip.daddr;
+  key.sport = udp.dest;
+  key.daddr = ip.saddr;
+  key.dport = udp.source;
+
+  struct dns_query_internal* cached = bpf_map_lookup_elem(&conns_map, &key);
+  if (!cached) {
+    return EXIT_FAILURE;
+  }
+
+  struct dns_query* out = bpf_ringbuf_reserve(&dns_queries_rb, sizeof(struct dns_query), 0);
+  if (!out) {
+    return EXIT_FAILURE;
+  }
+
+  out->id = cached->id;
+  out->pid = cached->pid;
+  out->tgid = cached->tgid;
+  out->cgroup_id = cached->cgroup_id;
+  out->latency_ns = bpf_ktime_get_ns() - cached->start_time;
+
+  bpf_probe_read_kernel_str(&out->name, NAME_BUF_SIZE, cached->name);
+  bpf_probe_read_kernel_str(&out->comm, COMM_BUF_SIZE, cached->comm);
+  bpf_probe_read_kernel_str(&out->cgroup, CGROUP_BUF_SIZE, cached->cgroup);
+
+  bpf_ringbuf_submit(out, 0);
   return EXIT_SUCCESS;
 }
 
